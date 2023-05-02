47d57f2d2c3ab802da8d0027df8f3b6828a64bf8
Ngozi Stephen <ngozi.stephen99@gmail.com>
2023-05-02 04:45:11 +0100

    Order New Parking Page

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,10 @@ const auth = useAuth();
           </h2>
  
           <template v-if="auth.check">
+            
+            <RouterLink class="router-link" :to="{ name: 'parkings.active' }">
+              Parkings
+            </RouterLink>
             <RouterLink class="router-link" :to="{ name: 'vehicles.index' }">
               Vehicles
             </RouterLink>
